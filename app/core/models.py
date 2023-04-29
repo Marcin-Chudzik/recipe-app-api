4c7971c57c744c42004516d4186294250deb5c39
Marcin Chudzik <marcinchudzik.webdeveloper@gmail.com>
2023-04-29 16:53:06 +0200

    check if test_models will pass on github actions.

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,11 @@
 Database models.
 """
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 
 class UserManager(BaseUserManager):
