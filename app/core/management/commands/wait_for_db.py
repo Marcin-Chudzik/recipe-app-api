f0300ed087a576b9ee9bedfbe6ee1838d268a309
Marcin Chudzik <marcinchudzik.webdeveloper@gmail.com>
2023-04-29 19:57:54 +0200

    checking if the result is working.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -14,14 +14,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database..., going to sleep')
+        self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
-                time.sleep(1)
+                self.stdout.write('Database unavailable, waiting 5 seconds...')
+                time.sleep(5)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
