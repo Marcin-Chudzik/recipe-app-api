6aaa705b84fe52dd7f6016c503cdb364ea453877
Marcin Chudzik <marcinchudzik.webdeveloper@gmail.com>
2023-04-29 18:33:45 +0200

    working on db connection refused.

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,13 +82,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME_TEST'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
     }
 }
 
